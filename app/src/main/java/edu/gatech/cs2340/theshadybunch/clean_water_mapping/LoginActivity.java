2a3d0f8ff77276245de193a73ef148844e33a51e
Priyesh Shah <ps.shah.priyesh@gmail.com>
2017-02-14 19:27:25 -0500

    Log In Page and Main Page working
    Log in and log out capabilities
    Log in requires "@" char or "user" as the email and >= 4 chars for the
    pass

--- app/src/main/java/edu/gatech/cs2340/theshadybunch/clean_water_mapping/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/theshadybunch/clean_water_mapping/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.gatech.cs2340.theshadybunch.clean_water_mapping;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -87,6 +88,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             @Override
             public void onClick(View view) {
                 attemptLogin();
+                Intent i = new Intent(getApplicationContext(), MainPageActivity.class);
+                startActivity(i);
             }
         });
 
@@ -143,9 +146,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    private void attemptLogin() {
+    private boolean attemptLogin() {
         if (mAuthTask != null) {
-            return;
+            return false;
         }
 
         // Reset errors.
@@ -181,23 +184,25 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // There was an error; don't attempt login and focus the first
             // form field with an error.
             focusView.requestFocus();
+            return false;
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            return true;
         }
     }
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return email.contains("@") || email.equals("user");
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.length() >= 4;
     }
 
     /**
